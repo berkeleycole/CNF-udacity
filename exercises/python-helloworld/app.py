66629cc40a6070e955e79793ef9bf70025561d10
berkeleycole <alyssamhope@gmail.com>
2021-06-09 21:20:34 -0700

    with solution - misunderstood which were supposed to be debug logs, but otherwise, not as far off as I had expected

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,40 +1,40 @@
 from flask import Flask
+from flask import json
 import logging
 
 app = Flask(__name__)
-root_logger= logging.getLogger()
-
-# Collect DEBUG level logs in app.log file
-logging.basicConfig(filename='app.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 # STATUS ENDPOINT
 @app.route("/status")
 def status():
-    logging.debug('status endpoint was reached')
     response = app.response_class(
-            response=json.dumps({"result":"OK - healthy"}),
-            status=200,
-            mimetype='application/json'
+        response=json.dumps({"result":"OK - healthy"}),
+        status=200,
+        mimetype='application/json'
     )
+    app.logger.info('/status endpoint success')
 
     return response
 
 # METRICS ENDPOINT
 @app.route("/metrics")
 def metrics():
-    logging.debug('metrics endpoint was reached')
     response = app.response_class(
-            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
-            status=200,
-            mimetype='application/json'
+        response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+        status=200,
+        mimetype='application/json'
     )
+    app.logger.info('/metrics endpoint success')
 
     return response
 
 # ROOT
 @app.route("/")
 def hello():
+    app.logger.info('root endpoint success')
     return "Hello World!"
 
 if __name__ == "__main__":
+    # Collect DEBUG level logs in app.log file
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0')
